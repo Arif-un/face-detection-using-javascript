35dc49dcde13818690fd6c40ece32868b0c71f82
Arif Uddin <arifunctg@gmail.com>
2020-04-10 18:00:36 +0600

    updated

--- index.php
+++ index.php
@@ -1,3 +1,86 @@
-<?php 
-header('Location:/index.html');
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Face Detection</title>
+
+  <link rel="stylesheet" href="style.css">
+
+  <script defer src="face-api.min.js"></script>
+  <script defer src="script.js"></script>
+</head>
+
+<body>
+  <div id="details">
+    <div>
+      Show face boundary
+      <label class="switch">
+        <input id="bound" type="checkbox">
+        <span class="slider round"></span>
+      </label>
+    </div>
+    <div>
+      Show face landmark
+      <label class="switch">
+        <input id="landmark" type="checkbox">
+        <span class="slider round"></span>
+      </label>
+    </div>
+    <div>
+      <div>
+        <small>Face Detection</small>
+        <div id="face">Detecting face...</div>
+      </div>
+      <div>
+        <small>Probability</small>
+        <div id="face-p">-</div>
+      </div>
+    </div>
+
+    <div>
+      <div>
+        <small>Gender</small>
+        <div id="gen">-</div>
+      </div>
+      <div>
+        <small>Probability</small>
+        <div id="gen-p">-</div>
+      </div>
+    </div>
+
+    <div>
+      <div>
+        <small>Age</small>
+        <div id="age">-</div>
+      </div>
+      <div>
+        <small>Probability</small>
+        <div id="gen-p">-</div>
+      </div>
+    </div>
+
+    <div>
+      <div>
+        <small>Expression</small>
+        <div id="exp">-</div>
+      </div>
+      <div>
+        <small>Probability</small>
+        <div id="exp-p">-</div>
+      </div>
+    </div>
+    <div id="exp-emo"></div>
+
+  </div>
+
+  <div id="alert">📷<small> Allow Web Cam to run this app</small></div>
+
+  <div id="cam-wrp">
+    <video id="cam" width="750" height="562" autoplay muted></video>
+  </div>
+
+</body>
+
+</html>
